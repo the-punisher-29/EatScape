dbbb022cca7460ba0091403ac25d6f511c1ff16c
Soumen <soumenkumar9503@gmail.com>
2024-06-22 12:20:23 +0530

    Contains res pertaining to Splash and Login Screens

--- app/src/main/java/in/sk/eatscape/LoginActivity.kt
+++ app/src/main/java/in/sk/eatscape/LoginActivity.kt
@@ -1,17 +1,26 @@
 package `in`.sk.eatscape
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import `in`.sk.eatscape.databinding.ActivityStartBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView9.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
